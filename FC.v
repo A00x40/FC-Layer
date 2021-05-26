844f22d98da318b3bcdfce912d10ebc96f2ff11d
A00x40 <Ahmedattia621@yahoo.com>
2021-05-26 20:22:27 +0200

    FC module initial

---
+++ FC.v
@@ -0,0 +1,18 @@
+module moduleName ( ramAddress , ramData );
+  
+
+input[15:0] ramAddress;
+input[7:0]  ramData;
+
+reg[7:0] firstByte;
+reg[7:0] secondByte;
+
+generate
+        
+endgenerate
+
+generate
+        
+endgenerate
+    
+endmodule
